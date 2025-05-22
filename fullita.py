1b0422b5423507326a04e95484ff352781d6c9a6
absinth19 <30318340+absinth19@users.noreply.github.com>
2025-05-22 21:34:18 +0200

    Update fullita.py

--- fullita.py
+++ fullita.py
@@ -11,8 +11,8 @@ from bs4 import BeautifulSoup
 import time
 
 
-MFPLINK = ""     # non mettere lo / finale al link
-MFPPSW = ""
+MFPLINK = "https://trimming-passaggio.hf.space"     # non mettere lo / finale al link
+MFPPSW = "Leggila"
 
 # Constants
 REFERER = "forcedtoplay.xyz"
